ec77dc78f3c2b35fde26061e54319fb917b77355
Irtisam Faruqui Alavi <161361386+Irtisam99@users.noreply.github.com>
2026-06-02 23:22:45 +0600

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_freq=collections.defaultdict(int)
+        t_freq=collections.defaultdict(int)
+
+        if len(s)!=len(t):
+            return False
+        
+        for i in range(len(s)):
+            s_freq[s[i]]+=1
+            t_freq[t[i]]+=1
+
+        if s_freq == t_freq:
+            return True
+        else:
+            return False
+
+        
